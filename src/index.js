74a8a59ff6c04eeb0dd2526a93e582f0a47266a7
Jessica <github email address>
2020-06-10 10:23:16 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const baseUrl = "http://localhost:3000/dogs"
+    const tables = document.querySelectorAll('.margin')
+    const dogTable = tables[4]
+
+    const fetchData = () => {
+        fetch(baseUrl)
+        .then(resp => resp.json())
+        .then(dogs => renderDogs(dogs))
+    };
+
+    const renderDogs = dogs => {
+        dogs.forEach(dog => {
+            renderDog(dog)
+        })
+    };
+
+    const renderDog = dog => {
+        const dogInfo = document.createElement('tr')
+        dogInfo.dataset.id = dog.id
+        dogInfo.innerHTML = `
+        <td>${dog.name}</td> <td>${dog.breed}</td> <td>${dog.sex}</td> <td><button class="edit" id=${dog.id}>Edit</button></td>
+        `
+        dogTable.appendChild(dogInfo)
+
+        const editButton = dogInfo.getElementsByTagName('button')
+        editButton.addEventListener('click', function(e){
+            
+        })
+    }
+
+    
+
+    fetchData();
 
 })
