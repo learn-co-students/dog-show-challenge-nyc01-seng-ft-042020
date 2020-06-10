4489c6461f0904fd098202ac985e781735e2dfb5
dnamgyal <don.namgyal@hotmail.com>
2020-06-10 14:42:44 -0400

    done

--- src/index.js
+++ src/index.js
@@ -17,31 +17,91 @@ function getDogs(){
     .then(r => r.json())
 }
 
+function getDog(dogId){
+    return fetch(`http://localhost:3000/dogs/${dogId}`)
+    .then(r => r.json())
+}
 
-function addDogToTable(dog){
-    const tableId = document.querySelector("#table-body")
-    //console.log(tableId)
-    tableId.innerHTML = " "
-    const tableRow = document.createElement("tr")
-    const name = document.createElement("td")
-    name.textContent = dog.name
-    const breed = document.createElement("td")
-    breed.textContent = dog.breed
-    const sex = document.createElement("td")
-    sex.textContent = dog.sex
-    const edit = document.createElement("td")
-    edit.innerHTML = `<button>Edit</button>`
-
-    tableId.append(tableRow.append(name, breed, sex, edit))
-
+function addDogsToTable(dogs){
+    dogs.forEach(dog => {
+        createDogTr(dog)
+    })
+    
 }
 
 
-//getDogs().then(dogs => addDogsToTable(dogs))
-getDogs().then(dogs => dogs.forEach(dog => addDogToTable(dog)))
+function createDogTr(dog){
+    const tableId = document.querySelector("#table-body")
+    tableId.innerHTML += ""
+    let dogTr = document.createElement("tr")
+    dogTr.dataset.id = dog.id
+    dogTr.innerHTML = ""
+
+    let dogName = document.createElement("td")
+    dogName.className = "padding center" 
+    dogName.textContent = dog.name
+
+    let dogBreed = document.createElement("td")
+    dogBreed.textContent = dog.breed
+    dogBreed.className = "padding center"
+
+    let dogSex = document.createElement("td")
+    dogSex.textContent = dog.sex
+    dogSex.className = "padding center"
+
+    let editDog = document.createElement("td")
+    editDog.className = "padding center"
+    editDog.innerHTML = `<button id="Edit Dog">Edit</button>`
+
+    dogTr.append(dogName, dogBreed, dogSex, editDog)
+    tableId.append(dogTr)
+
+    return dogTr
+
+}
+
+const form = document.getElementById("dog-form")
 
 
+document.addEventListener("click", function(e){
+    //console.log(e.target.parentNode.parentNode)
+    const dogId = e.target.parentNode.parentNode.dataset.id
+    //console.log(dogId)
+    if (e.target.id === "Edit Dog"){
+        getDog(dogId).then(dog => {
+            form.name.value = dog.name
+            form.dataset.id = dog.id
+            form.breed.value = dog.breed
+            form.sex.value = dog.sex
+        })
+    }
+})
 
+document.addEventListener("submit", function(e){
+    e.preventDefault()
+    const form = e.target
+    dogId = form.dataset.id
+    let updatedDog = {
+        name: form.name.value, breed: form.breed.value, sex: form.sex.value
+    }
 
+    fetch(`http://localhost:3000/dogs/${dogId}`, {
+        method: 'PATCH',
+        headers: {
+            "content-type": `application/json`,
+            accepts: 'application/json'
+        },
+        body: JSON.stringify(updatedDog)
+    }).then(r => r.json()).then(dog => displayDogs())
+
+})
+
+function displayDogs() {
+    const tableId = document.querySelector("#table-body")
+    tableId.innerHTML = ""
+    getDogs().then(dogs => addDogsToTable(dogs))
+}
+
+displayDogs()
 
 })
