3b6588fe057b6751b32c05e25bbde9e5088b9b6a
DarkLaughter <ramon.l.echeverria@gmail.com>
2020-06-10 10:23:31 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const dogsUrl = "http://localhost:3000/dogs"
+    const table = document.querySelector("#table-body")
+    const dogForm = document.querySelector("#dog-form")
 
+
+    function fecthDogs() {
+        fetch(dogsUrl)
+        .then(resp => resp.json())
+        .then(dogs => dogListing(dogs))
+    }
+
+    function dogListing(dogs) {
+        
+        dogs.forEach(dog => {
+           const newDogRow = document.createElement('TR') 
+           newDogRow.innerHTML = `
+           <tr>
+           <td>${dog.name}</td><td>${dog.breed}</td> <td>${dog.sex}</td> 
+           <td><button class='submit' id=${dog.id}>Edit</button></td>
+           </tr>
+           `
+           table.append(newDogRow)
+        });
+    }
+
+    table.addEventListener('click', (e) =>{
+        if (e.target.className === 'submit') {
+            fetch(`dogsUrl/e.target.id`)
+            .then(resp => resp.json())
+            .then(book => dogInfoForForm(book)) 
+        }
+
+    })
+
+
+    function name(params) {
+        
+    }
+
+    fecthDogs()
 })
