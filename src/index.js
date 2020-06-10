35cb6d22270a39189040632eaf108865c6cbb97e
DarkLaughter <ramon.l.echeverria@gmail.com>
2020-06-10 11:20:48 -0400

    more commits

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const dogsUrl = "http://localhost:3000/dogs"
     const table = document.querySelector("#table-body")
     const dogForm = document.querySelector("#dog-form")
+    
 
 
     function fecthDogs() {
@@ -17,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
            newDogRow.innerHTML = `
            <tr>
            <td>${dog.name}</td><td>${dog.breed}</td> <td>${dog.sex}</td> 
-           <td><button class='submit' id=${dog.id}>Edit</button></td>
+           <td><button class='edit' id=${dog.id}>Edit</button></td>
            </tr>
            `
            table.append(newDogRow)
@@ -25,18 +26,44 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     table.addEventListener('click', (e) =>{
-        if (e.target.className === 'submit') {
-            fetch(`dogsUrl/e.target.id`)
+        if (e.target.className === 'edit') {
+            fetch(`${dogsUrl}/${e.target.id}`)
             .then(resp => resp.json())
-            .then(book => dogInfoForForm(book)) 
+            .then(dog => dogInfoForForm(dog)) 
         }
 
     })
-
-
-    function name(params) {
-        
+    function dogInfoForForm(dog) {
+        dogForm.name.value = dog.name
+        dogForm.breed.value = dog.breed
+        dogForm.sex.value = dog.sex
     }
+
+    dogForm.addEventListener('submit', e => {
+        event.preventDefault();
+        if (e.target.id === 'dog-form') {
+            const name = e.target.name.value
+            const breed = e.target.breed.value
+            const sex = e.target.sex.value
+
+            fetch(`${dogsUrl}/${e.target.id}`, {
+            method: "PATCH",
+            headers: {
+                "content-type": "application/json",
+                "accept": "application/json"
+                },
+                body: JSON.stringify({ 
+                    name: name, 
+                    breed: breed,
+                    sex: sex
+                })
+                })
+                  .then(response => response.json())
+                  .then(dogs => console.log(dogs)
+                  )
+              
+        }
+    } )
 
     fecthDogs()
 })
